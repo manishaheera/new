64d1b41859c03fc00537018762a437502699c90a
Manisha Heera <nishahra@gmail.com>
2023-02-25 20:11:03 -0800

    first commit: client, server created. new project

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+
+//MIDDLE WARE FOR PUT AND POST CALLS, OTHERWISE WE WOULD GET TYPEERRORS
+//THIS PARSES INCOMING REQUESTS W/ JSON PAYLOADS
+// RECOGNIZES REQUEST OBJECT AS JSON OBJECT
+
+app.use(express.json());
+//PARSES INCOMING REQUESTS W/ JSON PAYLOADS AS STRINGS/ARRAYS.
+//RECOGNIZES REQUESTS OBJECTS AS STRINGS OR ARRAYS
+app.use(express.urlencoded({extended:true}));
+
+//LETS FRONT END PORT 3000 MAKE CALLS TO OUR BACK END PORT 8000
+// WITHOUT THIS, RESULTS WILL BE "CORS ERROR" WHEN MAKING AXIOS CALLS
+app.use(cors({
+    origin:"http://localhost:3000"
+}))
+
+// NEED TO REQUIRE CONFIG FILES ****************************
+// NEED TO REQUIRE ROUTES ***********************************
+
+app.listen(8000, ()=> console.log("You are connected to port 8000"))
+
+
